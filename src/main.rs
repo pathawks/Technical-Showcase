7e42237ce6972a6e955db3b1f3700a5d75da0e05
Pat Hawks <pat@pathawks.com>
2022-02-20 13:29:23 -0600

    Use arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fmt;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    album: Option<u64>,
+}
+
+impl fmt::Display for Cli {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.album {
+            Some(album_id) => write!(f, "album: {}", album_id),
+            None => write!(f, "No album specified"),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    println!("{}", args);
 }
